2ebb3a6ef862e81469269c641d6d3f2ccbe5a12b
Alokcseit <swainalokkumar25@gmail.com>
2025-09-16 22:29:47 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
-// import cors from 'cors';
+import cors from 'cors';
 dotenv.config({path:'./.env',quiet:true});
 const port=process.env.PORT || 3000;
 const app=express();
-// app.use(cors( {
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true
-// }));
+app.use(cors( {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
